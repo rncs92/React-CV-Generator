0d896bb075a570a2adf09c2a2abce10ad8cd5a77
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-08-28 17:11:03 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import useLocalStorage from "./hooks/useLocalStorage";
 
@@ -34,6 +34,8 @@ function App() {
     ""
   );
   const [softSkills, setSoftSkills] = useLocalStorage("softSkills", "");
+
+  const [languages, setLanguages] = useLocalStorage("languages", "");
 
   function handleNameInput(event) {
     setName(event.target.value);
@@ -105,6 +107,10 @@ function App() {
 
   function handleSoftSkillsInput(event) {
     setSoftSkills(event.target.value);
+  }
+
+  function handleLanguagesInput(event) {
+    setLanguages(event.target.value);
   }
 
   return (
@@ -500,6 +506,36 @@ function App() {
             />
           </div>
         </div>
+
+        <div className="p-4">
+          <h2 className="flex mr-2 text-xl font-bold font-sans mb-4">
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              viewBox="0 0 512 512"
+              className="mr-2 h-6 w-6 text-gray-600"
+              >
+              <path d="M57.7 193l9.4 16.4c8.3 14.5 21.9 25.2 38 29.8L163 255.7c17.2 4.9 29 20.6 29 38.5v39.9c0 11 6.2 21 16 25.9s16 14.9 16 25.9v39c0 15.6 14.9 26.9 29.9 22.6c16.1-4.6 28.6-17.5 32.7-33.8l2.8-11.2c4.2-16.9 15.2-31.4 30.3-40l8.1-4.6c15-8.5 24.2-24.5 24.2-41.7v-8.3c0-12.7-5.1-24.9-14.1-33.9l-3.9-3.9c-9-9-21.2-14.1-33.9-14.1H257c-11.1 0-22.1-2.9-31.8-8.4l-34.5-19.7c-4.3-2.5-7.6-6.5-9.2-11.2c-3.2-9.6 1.1-20 10.2-24.5l5.9-3c6.6-3.3 14.3-3.9 21.3-1.5l23.2 7.7c8.2 2.7 17.2-.4 21.9-7.5c4.7-7 4.2-16.3-1.2-22.8l-13.6-16.3c-10-12-9.9-29.5 .3-41.3l15.7-18.3c8.8-10.3 10.2-25 3.5-36.7l-2.4-4.2c-3.5-.2-6.9-.3-10.4-.3C163.1 48 84.4 108.9 57.7 193zM464 256c0-36.8-9.6-71.4-26.4-101.5L412 164.8c-15.7 6.3-23.8 23.8-18.5 39.8l16.9 50.7c3.5 10.4 12 18.3 22.6 20.9l29.1 7.3c1.2-9 1.8-18.2 1.8-27.5zm48 0c0 141.4-114.6 256-256 256S0 397.4 0 256S114.6 0 256 0S512 114.6 512 256z"></path>
+            </svg>
+            Languages
+          </h2>
+          <div className="flex flex-col w-full mb-2">
+            <label
+              htmlFor="company"
+              className="mb-2 text-lg font-sans font-medium"
+            >
+              Languages
+            </label>
+            <input
+              name="company"
+              id="company"
+              value={languages}
+              onChange={handleLanguagesInput}
+              type="text"
+              placeholder="Technical skills"
+              className="mt-1 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+            />
+          </div>
+        </div>
       </div>
 
       <div className="w-1/2 bg-white p-4">
@@ -518,15 +554,17 @@ function App() {
               <p className="font-sm">{workYears}</p>
             </div>
             <div className="flex">
-              {jobTitle? (
-            <p className="font-sm font-medium">{jobTitle},</p>
-            ) : (
-              <p></p>
-            )}
-            <p className="ml-2 font-sm italic">{company}</p>
+              {jobTitle ? (
+                <p className="font-sm font-medium">{jobTitle},</p>
+              ) : (
+                <p></p>
+              )}
+              <p className="ml-2 font-sm italic">{company}</p>
             </div>
             <div className="flex">
-              <p className="ml-4 mt-2 font-sm text-justify">{workDescription}</p>
+              <p className="ml-4 mt-2 font-sm text-justify">
+                {workDescription}
+              </p>
             </div>
           </div>
         </div>
@@ -670,6 +708,13 @@ function App() {
           ) : (
             <p></p>
           )}
+        </div>
+
+        <div className="px-14 py-4">
+          <h3 className="w-1/4 text-xl text-white border-b-4 border-white">
+            Languages
+          </h3>
+          <p className="mt-2 text-white">{languages}</p>
         </div>
       </div>
     </div>
